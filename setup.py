d34bdf1ba2d3e2c3574481422a073223dbbc110e
ddb324 <jiangdaoran@163.com>
2023-04-26 21:10:40 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='mynnunet',
+      packages=find_namespace_packages(include=["mynnunet", "mynnunet.*"]),
+      version='0.0.1',
+      description='mynnU-Net',
+      author_email='jiangdaoran@163.com',
+      license='Apache License Version 2.0, January 2004',
+      python_requires=">=3.9",
+      install_requires=[
+          "torch>=2.0.0",
+          "acvl-utils>=0.2",
+          "dynamic-network-architectures>=0.2",
+          "tqdm",
+          "dicom2nifti",
+          "scikit-image>=0.14",
+          "medpy",
+          "scipy",
+          "batchgenerators>=0.25",
+          "numpy",
+          "scikit-learn",
+          "scikit-image>=0.19.3",
+          "SimpleITK>=2.2.1",
+          "pandas",
+          "graphviz",
+          'tifffile',
+          'requests',
+          "nibabel",
+          "matplotlib",
+          "seaborn",
+          "imagecodecs",
+          "yacs"
+      ],
+      entry_points={
+          'console_scripts': [
+              'nnUNetv2_train = nnunetv2.run.run_training:run_training_entry',  # api available
+          ],
+      },
+      keywords=['deep learning', 'image segmentation', 'medical image analysis',
+                'medical image segmentation', 'nnU-Net', 'nnunet']
+      )
